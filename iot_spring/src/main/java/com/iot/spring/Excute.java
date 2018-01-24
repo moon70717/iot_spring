7931125f789c8820330b6f1845a0c794985553e5
DJA <DJA@DJA-PC>
2018-01-24 18:43:12 +0900

    123123

--- iot_spring/src/main/java/com/iot/spring/Excute.java
+++ iot_spring/src/main/java/com/iot/spring/Excute.java
@@ -8,10 +8,8 @@ public class Excute {
 
 	public static void main(String[]args) {
 		Resource res= new ClassPathResource("beans.xml");
-		System.out.println("beans.xml 파싱완료");
 		XmlBeanFactory bf=new XmlBeanFactory(res);
-		HelloSpring hs=(HelloSpring)bf.getBean("hw");
-		System.out.println(hs.getMessage());
-		System.out.println(hs.getName());
+		Test t=(Test)bf.getBean("test");
+		t.printTest();
 	}
 }
